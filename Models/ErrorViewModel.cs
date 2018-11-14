8aabc9f7e4ea11b732c19a97974013c9edfb8533
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2018-11-13 22:36:55 -0300

    Commit inicial con la aplicación por defecto de asp.net core

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EscuelaAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
